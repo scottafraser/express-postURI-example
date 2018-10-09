2cefcc0763f87064818460c59ddc3bd310ef4b58
Scott Fraser <scottafraser@yahoo.com>
2018-10-09 10:13:12 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// grab the packages we need
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 8080;
+
+// routes will go here
+app.get("/api/users", function(req, res) {
+  var user_id = req.param("id");
+  var token = req.param("token");
+  var geo = req.param("geo");
+
+  res.send(user_id + " " + token + " " + geo);
+});
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// POST http://localhost:8080/api/users
+// parameters sent with
+app.post("/api/users", function(req, res) {
+  var user_id = req.body.id;
+  var token = req.body.token;
+  var geo = req.body.geo;
+
+  res.send(user_id + " " + token + " " + geo);
+});
+
+// start the server
+app.listen(port);
+console.log("Server started! At http://localhost:" + port);
